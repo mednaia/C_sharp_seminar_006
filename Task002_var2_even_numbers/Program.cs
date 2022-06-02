ce80f9de101f6cd942012d1bf0e45956ca17d4f8
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-06-02 22:39:50 +0300

    Task 002 use function to change even numbers

---
+++ Task002_var2_even_numbers/Program.cs
@@ -0,0 +1,47 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+void FillArray(int[,] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(-100,101);
+        }
+    }
+}
+//--------------------------------------------------------------------------------------------+
+void ChangeEvenElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if(array[i,j]%2 == 0) array[i,j] = -array[i,j];
+            }
+    }
+}
+//--------------------------------------------------------------------------------------------+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+    Console.WriteLine();
+    }
+}
+
+Console.Write("Input the number of strings of array: ");
+int n = int.Parse(Console.ReadLine()??"0");
+Console.Write("Input the number of columns of array: ");
+int k = int.Parse(Console.ReadLine()??"0");
+int[,]array = new int[n,k];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeEvenElements(array);
+PrintArray(array);
