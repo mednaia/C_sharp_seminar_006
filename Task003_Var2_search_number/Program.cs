1f10cf169e0eae2c83fd81dcf1e6640562db41c0
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-06-03 10:43:42 +0300

    Task 003 Variant 2 search number in array

---
+++ Task003_Var2_search_number/Program.cs
@@ -0,0 +1,45 @@
+﻿// В двумерном массиве показать позицию числа, заданного пользователем или указать, что такого элемента нет
+void PrintMatrix(int[,] matrix)
+{
+    for(int i=0; i<matrix.GetLength(0); i++)
+    {
+        for(int j=0; j<matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i,j]} ");
+    Console.WriteLine();
+    }
+}
+int[,] CreateMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rows,columns];
+    var random = new Random();
+    for(int i=0;i<rows;i++)
+        for(int j=0;j<columns;j++)
+            matrix[i,j] = random.Next(minValue,maxValue);
+    return matrix;
+}
+int[]? IndexOf(int[,] matrix, int value)
+{
+    for(int i=0; i<matrix.GetLength(0);i++)
+        for(int j=0; j<matrix.GetLength(0);j++)
+            if(matrix[i,j]==value)
+                return new int[2] {i,j};
+    return null;
+}
+Console.Write("Enter amount of matrix rows: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter amount of matrix columns: ");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter left side of matrix columns: ");
+int start = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter right side of matrix columns: ");
+int finish = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = CreateMatrix(rows,columns,start,finish);
+Console.WriteLine("Your matrix: ");
+PrintMatrix(matrix);
+Console.Write("Enter value to find in matrix: ");
+int value = int.Parse(Console.ReadLine() ?? "0");
+int[]? position = IndexOf(matrix,value);
+if(position==null)
+    Console.Write("There is no value in matrix.");
+else
+    Console.Write($"Position of element: {position[0]}  {position[1]}.");
